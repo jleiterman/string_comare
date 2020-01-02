f8ab3fbf4bf902382a50a6423c299fc974acf17c
Justin Leiterman <justin.leiterman@uscellular.com>
2020-01-02 16:03:52 -0600

    adding support for multiple randomly selected string

--- test2.py
+++ test2.py
@@ -1,6 +1,3 @@
-
-
-
 import random
 def get_hint(fullset,revealedset):
     seed_length = random.randrange(20,40)
@@ -9,21 +6,21 @@ def get_hint(fullset,revealedset):
         seed_length = max(fullset) - seed_start
     return revealedset.union(set(range(seed_start,seed_start+seed_length)))
 
-def printrevealedstring(longstring,fullset,revealedset):
+def printrevealedstring(shortstring,fullset,revealedset):
     printstring = ''
     for i in fullset:
         if i in revealedset:
-            printstring = printstring+longstring[i]
+            printstring = printstring+shortstring[i]
         else:
             printstring = printstring+'*'
     print(printstring)
 
 import difflib
 
-def evaluateinputstring(inputstring,longstring,fullset,revealedset):
-    s = difflib.SequenceMatcher(None, inputstring, longstring, autojunk=False)
+def evaluateinputstring(inputstring,shortstring,fullset,revealedset):
+    s = difflib.SequenceMatcher(None, inputstring, shortstring, autojunk=False)
     blocks = s.get_matching_blocks()
-    matchedstring = longstring[blocks[0][1]:blocks[-2][1]+blocks[-2][2]]
+    matchedstring = shortstring[blocks[0][1]:blocks[-2][1]+blocks[-2][2]]
     s2= difflib.SequenceMatcher(None, inputstring, matchedstring, autojunk=False)
     print("match Ratio:" +str(s2.ratio()))
     if s2.ratio() > .9:
@@ -42,22 +39,29 @@ os.system('clear')
 with open('longstring', 'r') as file:
 #    data = file.read().replace('\n', '')
     longstring = file.read()
+stringsplit = longstring.split("\n")
+continue_var = True
 
-fullset = set(range(0,len(longstring)-1))
-revealedset = set()
-for i in fullset:
-    if longstring[i] == ' ' or longstring[i] in string.punctuation:
-        revealedset = revealedset.union(set([i]))
-
-printrevealedstring(longstring, fullset,revealedset)
-while fullset != revealedset:
-    inputstring = input("fill in the blanks: ") 
+while continue_var:
+    shortstring = random.choice(stringsplit)
+    fullset = set(range(0,len(shortstring)-1))
+    revealedset = set()
+    for i in fullset:
+        if shortstring[i] == ' ' or shortstring[i] in string.punctuation:
+            revealedset = revealedset.union(set([i]))
+    printrevealedstring(shortstring, fullset,revealedset)
+    while fullset != revealedset:
+        inputstring = input("fill in the blanks: ") 
+        os.system('clear')
+        if inputstring == '':
+            revealedset = get_hint(fullset,revealedset)
+        else:
+            revealedset = evaluateinputstring(inputstring,shortstring,fullset,revealedset)
+        printrevealedstring(shortstring, fullset,revealedset)
+    #continueinuput = input("Want to continue (y/n): ")
+    if input("Want to continue (y/n): ") == 'n':
+        continue_var = False
     os.system('clear')
-    if inputstring == '':
-        revealedset = get_hint(fullset,revealedset)
-    else:
-        revealedset = evaluateinputstring(inputstring,longstring,fullset,revealedset)
-    printrevealedstring(longstring, fullset,revealedset)
-
+      
 
     
